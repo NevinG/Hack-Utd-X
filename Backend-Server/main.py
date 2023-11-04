4197d537ebfd63620486bb27f285d0c94d9788b9
Nevin Gilday <nevingilday@gmail.com>
2023-11-04 16:27:20 -0500

    added stuff to dashboard

--- Backend-Server/main.py
+++ Backend-Server/main.py
@@ -1,11 +1,29 @@
 import os
+import firebase_admin
 from flask_cors import CORS, cross_origin
 
 from flask import Flask, request
+
+
+from firebase_admin import credentials, firestore, auth
+
+cred = credentials.Certificate("./authenticationKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+#Checks if user is authenticated
+def check_auth():
+    token = request.headers.get("Authtoken", None)
+    if token == None:
+        return ""
+    decoded_token = auth.verify_id_token(token)    
+    uid = decoded_token['uid']
+    return uid
 
 #Checks if user is authenticated
 @app.route('/user', methods=['GET'])
