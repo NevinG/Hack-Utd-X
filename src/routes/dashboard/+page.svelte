dbb8e4567ec1799c76f58b65a82f18d3466572a3
md-y <15069105+md-y@users.noreply.github.com>
2023-11-05 08:32:46 -0600

    Icons for AR/Map and fix them

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,14 +2,23 @@
 	import { signOut } from 'firebase/auth';
 	import { auth, sampleProperty } from '../../util.js';
 	import { userData, userExists } from '../../store.js';
+	import LoadingIcon from '$lib/components/LoadingIcon.svelte';
 	import { base } from '$app/paths';
 	import { page } from '$app/stores';
+
+	import MapIcon from '~icons/material-symbols/location-on';
+	import CameraIcon from '~icons/material-symbols/camera';
 
 	function logout() {
 		signOut(auth);
 		location.replace('/');
 	}
 </script>
+
+<div id="overlay">
+	<a href="{base}/map"><MapIcon /></a>
+	<a href="{base}/ar"><CameraIcon /></a>
+</div>
 
 {#if $userExists}
 	<div id="wrapper">
@@ -41,10 +50,28 @@
 		<button on:click={logout}>logout</button>
 	</div>
 {:else}
-	<p>Cant get data :(</p>
+	<div id="loading-container">
+		<LoadingIcon />
+	</div>
 {/if}
 
 <style>
+	#loading-container {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	#overlay {
+		position: absolute;
+		left: 1rem;
+		top: 1rem;
+		height: 2rem;
+		font-size: 2rem;
+	}
+
 	#wrapper {
 		display: flex;
 		justify-content: center;
@@ -56,9 +83,12 @@
 		padding: 25px;
 		margin: 5px;
 		background-color: rgb(82, 129, 133);
+		width: 100%;
 	}
 	.property-parent {
 		display: flex;
+		width: 100%;
+		max-width: 400px;
 	}
 	.button-container {
 		display: flex;
