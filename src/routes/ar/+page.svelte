e9e7bd8e0241d5ab2cc32c4d2a6006258bc89669
md-y <15069105+md-y@users.noreply.github.com>
2023-11-04 22:49:15 -0500

    Added photo frontend

--- src/routes/ar/+page.svelte
+++ src/routes/ar/+page.svelte
@@ -10,6 +10,7 @@
 
 	import HomeIcon from '~icons/material-symbols/home';
 	import CloseIcon from '~icons/carbon/close';
+	import CameraIcon from '~icons/material-symbols/android-camera';
 
 	import ArThreeUrl from '@ar-js-org/ar.js/three.js/build/ar-threex-location-only?url';
 	import ArAFrameUrl from '@ar-js-org/ar.js/aframe/build/aframe-ar?url';
@@ -46,6 +47,8 @@
 		}
 	}
 
+	let photoCanvas;
+
 	function onLibraryLoad() {
 		loadCount++;
 		console.log('load count:', loadCount);
@@ -58,6 +61,28 @@
 			console.log('lat:', lat);
 			console.log('long:', long);
 		});
+	}
+
+	let photoPromise = null;
+	async function takePhoto() {
+		const videos = document.getElementsByTagName('video');
+		if (videos.length === 0) return;
+		const video = videos[videos.length - 1];
+
+		photoCanvas.width = video.videoWidth * 2;
+		photoCanvas.height = video.videoHeight * 2;
+
+		const ctx = photoCanvas.getContext('2d');
+		ctx.drawImage(video, 0, 0, photoCanvas.width, photoCanvas.height);
+		const data = photoCanvas.toDataURL('image/png');
+		const res = await fetch('http://localhost:3030/predict', {
+			method: 'POST',
+			body: JSON.stringify({
+				image: data
+			})
+		});
+		const payload = await res.json();
+		return payload.image;
 	}
 
 	onMount(() => {
@@ -89,89 +114,162 @@
 	{/if}
 </svelte:head>
 
-{#if ready}
-	<a-scene
-		vr-mode-ui="enabled: false"
-		arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false"
-		renderer="antialias: true; alpha: true"
-	>
-		<a-camera
-			id="camera"
-			gps-new-camera="gpsMinDistance: 5;"
-			on:gps-camera-update-position={(data) => {
-				const pos = data.detail.position;
-				lat = pos.latitude;
-				long = pos.longitude;
-				console.log('GPS update:', lat, long);
-			}}
-			arjs-device-orientation-controls="smoothingFactor: 0.1"
-			cursor="rayOrigin: mouse"
+<div class:hidden={photoPromise != null}>
+	{#if ready}
+		<a-scene
+			vr-mode-ui="enabled: false"
+			arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false"
+			renderer="antialias: true; alpha: true"
 		>
-			<a-entity
-				cursor="fuse: true; fuseTimeout: 1"
-				position="0 0 -1"
-				geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
-				material="color: white; shader: flat"
-			/>
-		</a-camera>
-		{#if assets !== null}
-			{#each assets.filter((a) => a.floor === selectedFloor) as asset}
-				<ArAsset {asset} />
-			{/each}
-		{/if}
-	</a-scene>
-	<div id="ar-overlay">
-		<div class="header">
-			<a href="/">
-				<HomeIcon />
-			</a>
-			<select id="property-select" bind:value={selectedProperty}>
-				{#each propertyNames as name}
-					<option value={name}>{name}</option>
-				{/each}
-			</select>
-			<select id="floor-select" bind:value={selectedFloor}>
-				{#each floors as floor}
-					<option value={floor}>{floor}</option>
-				{/each}
-			</select>
-		</div>
-		{#if $currentAsset !== null}
-			<div
-				class="footer"
-				transition:fly={{
-					duration: 300,
-					y: 500
+			<a-camera
+				id="camera"
+				gps-new-camera="gpsMinDistance: 5;"
+				on:gps-camera-update-position={(data) => {
+					const pos = data.detail.position;
+					lat = pos.latitude;
+					long = pos.longitude;
+					console.log('GPS update:', lat, long);
 				}}
+				arjs-device-orientation-controls="smoothingFactor: 0.1"
+				cursor="rayOrigin: mouse"
 			>
-				<div class="close" on:click={() => ($currentAsset = null)} role="none">
-					<CloseIcon />
-				</div>
-				<div class="body">
-					<h2><b>{$currentAsset.name}</b></h2>
-					<h2>{$currentAsset.description}</h2>
-					<h2>${formatValue($currentAsset.value ?? 0)}</h2>
-					<br />
-					<h2>
-						{Math.round(
-							distanceBetweenLatLong(
-								lat,
-								long,
-								$currentAsset.location.lat,
-								$currentAsset.location.long
-							) * 100
-						) / 100} meters away
-					</h2>
-					<span class="spacer" />
-				</div>
+				<a-entity
+					cursor="fuse: true; fuseTimeout: 1"
+					position="0 0 -1"
+					geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
+					material="color: white; shader: flat"
+				/>
+			</a-camera>
+			{#if assets !== null}
+				{#each assets.filter((a) => a.floor === selectedFloor) as asset}
+					<ArAsset {asset} />
+				{/each}
+			{/if}
+		</a-scene>
+		<div id="ar-overlay">
+			<div class="header">
+				<a href="/">
+					<HomeIcon />
+				</a>
+				<select id="property-select" bind:value={selectedProperty}>
+					{#each propertyNames as name}
+						<option value={name}>{name}</option>
+					{/each}
+				</select>
+				<select id="floor-select" bind:value={selectedFloor}>
+					{#each floors as floor}
+						<option value={floor}>{floor}</option>
+					{/each}
+				</select>
 			</div>
-		{/if}
+			<div class="photo-button" on:click={() => (photoPromise = takePhoto())} role="none">
+				<CameraIcon />
+			</div>
+			{#if $currentAsset !== null}
+				<div
+					class="footer"
+					transition:fly={{
+						duration: 300,
+						y: 500
+					}}
+				>
+					<div class="close" on:click={() => ($currentAsset = null)} role="none">
+						<CloseIcon />
+					</div>
+					<div class="body">
+						<h2><b>{$currentAsset.name}</b></h2>
+						<h2>{$currentAsset.description}</h2>
+						<h2>${formatValue($currentAsset.value ?? 0)}</h2>
+						<br />
+						<h2>
+							{Math.round(
+								distanceBetweenLatLong(
+									lat,
+									long,
+									$currentAsset.location.lat,
+									$currentAsset.location.long
+								) * 100
+							) / 100} meters away
+						</h2>
+						<span class="spacer" />
+					</div>
+				</div>
+			{/if}
+		</div>
+	{:else}
+		<LoadingIcon />
+	{/if}
+	<canvas bind:this={photoCanvas} class="hidden" />
+</div>
+{#if photoPromise}
+	<div id="photo-overlay">
+		<div class="header" on:click={() => (photoPromise = null)} role="none">
+			<CloseIcon />
+		</div>
+		{#await photoPromise}
+			<div class="center">
+				<LoadingIcon />
+			</div>
+		{:then dataString}
+			<div class="scroll">
+				<img src={dataString} alt="Your webcam with classifications" />
+			</div>
+		{:catch error}
+			<div class="center">
+				<p>{error}</p>
+			</div>
+		{/await}
 	</div>
-{:else}
-	<LoadingIcon />
 {/if}
 
 <style lang="scss">
+	:global(video) {
+		display: none;
+	}
+
+	.hidden {
+		display: none;
+	}
+
+	#photo-overlay {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+
+		display: flex;
+
+		flex-direction: column;
+
+		.header {
+			position: absolute;
+			right: 2rem;
+			top: 2rem;
+
+			:global(svg) {
+				font-size: 2rem;
+			}
+		}
+
+		.center {
+			height: 100%;
+			width: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+
+		.scroll {
+			padding: 4rem;
+			box-sizing: border-box;
+
+			overflow-y: auto;
+
+			img {
+				width: 100%;
+			}
+		}
+	}
+
 	#ar-overlay {
 		position: absolute;
 		left: 0;
@@ -231,6 +329,21 @@
 				display: block;
 				width: 100%;
 				height: 10vh;
+			}
+		}
+
+		.photo-button {
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			width: 100%;
+			height: 10rem;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			:global(svg) {
+				font-size: 2rem;
 			}
 		}
 	}
