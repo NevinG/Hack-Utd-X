ad0e3c140543db19b35acfa855cbeac7ca66c9d8
danielsuhcompsci <danielsuh112@yahoo.com>
2023-11-05 07:52:05 -0600

    changes to property page UI, added font, added svelte-typewriter package

--- src/routes/dashboard/property/+page.svelte
+++ src/routes/dashboard/property/+page.svelte
@@ -10,16 +10,18 @@
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
 	import { base } from '$app/paths';
+	import LoadingIcon from '$lib/components/LoadingIcon.svelte';
+	import Typewriter from 'svelte-typewriter';
 
-	let assetDropdown = false;
-	let featuresDropdown = false;
-	let defectLogDropdown = false;
-	let rennovationLogDropdown = false;
+	let assetDropdown = true;
+	let featuresDropdown = true;
+	let defectLogDropdown = true;
+	let rennovationLogDropdown = true;
 
-	let environmentalReport = {};
-	let narrative = {};
+	let environmentalReportPromise = null;
+	let narrativeReportPromise = null;
 	let predictCondition = {};
-	let predictValue = {}
+	let predictValue = {};
 
 	let id;
 
@@ -30,302 +32,376 @@
 		}
 
 		predictCondition = await getPredictCondition($userData.properties[id]);
-		console.log(predictCondition)
-		
+		console.log(predictCondition);
+
 		predictValue = await getPredictValueOverTime($userData.properties[id]);
-		console.log(predictValue)
+		console.log(predictValue);
 
-		environmentalReport = await getEnvironmentalReport($userData.properties[id]);
-		environmentalReport = environmentalReport['message'];
+		environmentalReportPromise = getEnvironmentalReport($userData.properties[id]);
+		// environmentalReport = environmentalReport['message'];
 
-		narrative = await getNarrative($userData.properties[id]);
-		narrative = narrative['message'];
+		narrativeReportPromise = getNarrative($userData.properties[id]);
+		// narrative = narrative['message'];
 	});
 </script>
 
 <!-- TODO IS TO get dat on back if it doesnt save -->
 {#if id}
-	<a href="{base}/dashboard">Back</a>
 	<div id="title-wrapper">
-		<div>
+		<div style="display: flex; justify-content: space-around; align-items: center;">
+			<a href="{base}/dashboard">Back</a>
 			<input
 				placeholder="(name here)"
 				class="input-title"
 				bind:value={$userData.properties[id].name}
 			/>
 		</div>
-		<img src="/{$userData.properties[id].picture}" alt="commercial real estate property" />
-		<br />
-		<div class="bubble">
-			<div class="bubble-child">
-				Size: <input placeholder="(sq-ft here)" bind:value={$userData.properties[id]['sq-ft']} />
-			</div>
-			<div class="bubble-child">
-				Built: <input
-					placeholder="(build date here)"
-					bind:value={$userData.properties[id]['built-date']}
-				/>
-			</div>
-			<div class="bubble-child">
-				Value: $<input placeholder="(value here)" bind:value={$userData.properties[id]['value']} />
-			</div>
-			<div class="bubble-child">
-				Address: $<input
-					placeholder="(address here)"
-					bind:value={$userData.properties[id]['address']}
-				/>
+		<div style="display: flex; flex-direction: row; ">
+			<img src="/{$userData.properties[id].picture}" alt="commercial real estate property" />
+			<div style="display: flex; flex-direction: column; justify-content: center;">
+				<div class="bubble" style="display: flex; flex-direction: column; justify-content: center;">
+					<div class="bubble-child">
+						Size: <input
+							placeholder="(sq-ft here)"
+							bind:value={$userData.properties[id]['sq-ft']}
+						/>
+					</div>
+					<div class="bubble-child">
+						Built: <input
+							placeholder="(build date here)"
+							bind:value={$userData.properties[id]['built-date']}
+						/>
+					</div>
+					<div class="bubble-child">
+						Value: $<input
+							placeholder="(value here)"
+							bind:value={$userData.properties[id]['value']}
+						/>
+					</div>
+					<div class="bubble-child">
+						Address: $<input
+							placeholder="(address here)"
+							bind:value={$userData.properties[id]['address']}
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
+		<div class="grid">
+			<div class="container grid-item">
+				<h1>
+					Assets <button
+						on:click={() => {
+							assetDropdown = !assetDropdown;
+						}}>+</button
+					>
+				</h1>
+				{#if assetDropdown}
+					<div class="dropdown">
+						{#each $userData.properties[id].assets as asset, i}
+							<div style="display: flex; align-items: center;">
+								<div class="bubble">
+									<div class="bubble-child">
+										Name: <input
+											placeholder="(name here)"
+											bind:value={$userData.properties[id].assets[i]['name']}
+										/>
+									</div>
+									<div class="bubble-child">
+										Value: $<input
+											placeholder="(value here)"
+											bind:value={$userData.properties[id].assets[i]['value']}
+										/>
+									</div>
+									<div class="bubble-child">
+										Description: <input
+											placeholder="(description here)"
+											bind:value={$userData.properties[id].assets[i]['description']}
+										/>
+									</div>
+									<div class="bubble-child">
+										Latitude: <input
+											placeholder="(lat here)"
+											bind:value={$userData.properties[id].assets[i]['location']['lat']}
+										/>
+									</div>
+									<div class="bubble-child">
+										Longitude: <input
+											placeholder="(long here)"
+											bind:value={$userData.properties[id].assets[i]['location']['long']}
+										/>
+									</div>
+								</div>
+								<button
+									style="height: 50%"
+									on:click={() => {
+										$userData.properties[id].assets.splice(i, 1);
+										$userData = $userData;
+									}}>X</button
+								>
+							</div>
+						{/each}
+					</div>
+					<br />
+					<button
+						on:click={() => {
+							$userData.properties[id].assets.push({
+								name: '',
+								value: '',
+								description: '',
+								location: {
+									lat: '',
+									long: ''
+								}
+							});
+							$userData = $userData;
+						}}>Add New</button
+					>
+				{/if}
+			</div>
+			<div class="container grid-item">
+				<h1>
+					Interior & Exterior <button
+						on:click={() => {
+							featuresDropdown = !featuresDropdown;
+						}}>+</button
+					>
+				</h1>
+				{#if featuresDropdown}
+					{#each $userData.properties[id]['features'] as feature, i}
+						<div style="display: flex; align-items: center;">
+							<div class="bubble">
+								<div class="bubble-child">
+									Name: <input
+										placeholder="(name here)"
+										bind:value={$userData.properties[id]['features'][i]['name']}
+									/>
+								</div>
+								<div class="bubble-child">
+									Type: <input
+										placeholder="(type here)"
+										bind:value={$userData.properties[id]['features'][i]['type']}
+									/>
+								</div>
+								<div class="bubble-child">
+									Description: <input
+										placeholder="(description here)"
+										bind:value={$userData.properties[id]['features'][i]['description']}
+									/>
+								</div>
+							</div>
+							<button
+								style="height: 50%"
+								on:click={() => {
+									$userData.properties[id]['features'].splice(i, 1);
+									$userData = $userData;
+								}}>X</button
+							>
+						</div>
+					{/each}
+					<br />
+					<button
+						on:click={() => {
+							$userData.properties[id]['features'].push({
+								name: '',
+								type: '',
+								description: ''
+							});
+							$userData = $userData;
+						}}>Add New</button
+					>
+				{/if}
+			</div>
+			<div class="container grid-item">
+				<h1>
+					Defect Log <button
+						on:click={() => {
+							defectLogDropdown = !defectLogDropdown;
+						}}>+</button
+					>
+				</h1>
 
-		<div class="container">
-			<h1>
-				Assets <button
-					on:click={() => {
-						assetDropdown = !assetDropdown;
-					}}>+</button
-				>
-			</h1>
-			{#if assetDropdown}
-				{#each $userData.properties[id].assets as asset, i}
-					<div style="display: flex; align-items: center;">
-						<div class="bubble">
-							<div class="bubble-child">
-								Name: <input
-									placeholder="(name here)"
-									bind:value={$userData.properties[id].assets[i]['name']}
-								/>
+				{#if defectLogDropdown}
+					{#each $userData.properties[id]['defect-log'] as defectLog, i}
+						<div style="display: flex; align-items: center;">
+							<div class="bubble">
+								<div class="bubble-child">
+									<input
+										placeholder="(description here)"
+										bind:value={$userData.properties[id]['defect-log'][i]['description']}
+									/>
+								</div>
+								<div class="bubble-child">
+									<input
+										placeholder="(date here)"
+										bind:value={$userData.properties[id]['defect-log'][i]['date']}
+									/>
+								</div>
+								<div class="bubble-child">
+									$<input
+										placeholder="(repair cost here)"
+										bind:value={$userData.properties[id]['defect-log'][i]['repair-cost']}
+									/>
+								</div>
 							</div>
-							<div class="bubble-child">
-								Value: $<input
-									placeholder="(value here)"
-									bind:value={$userData.properties[id].assets[i]['value']}
-								/>
-							</div>
-							<div class="bubble-child">
-								Description: <input
-									placeholder="(description here)"
-									bind:value={$userData.properties[id].assets[i]['description']}
-								/>
-							</div>
-							<div class="bubble-child">
-								Latitude: <input
-									placeholder="(lat here)"
-									bind:value={$userData.properties[id].assets[i]['location']['lat']}
-								/>
-							</div>
-							<div class="bubble-child">
-								Longitude: <input
-									placeholder="(long here)"
-									bind:value={$userData.properties[id].assets[i]['location']['long']}
-								/>
-							</div>
+							<button
+								style="height: 50%"
+								on:click={() => {
+									$userData.properties[id]['defect-log'].splice(i, 1);
+									$userData = $userData;
+								}}>X</button
+							>
 						</div>
-						<button
-							style="height: 50%"
-							on:click={() => {
-								$userData.properties[id].assets.splice(i, 1);
-								$userData = $userData;
-							}}>X</button
-						>
-					</div>
-				{/each}
-				<br />
-				<button
-					on:click={() => {
-						$userData.properties[id].assets.push({
-							name: null,
-							value: null,
-							description: null
-						});
-						$userData = $userData;
-					}}>Add New</button
-				>
-			{/if}
+					{/each}
+					<br />
+					<button
+						on:click={() => {
+							$userData.properties[id]['defect-log'].push({
+								description: '',
+								date: '',
+								'repair-cost': ''
+							});
+							$userData = $userData;
+						}}>Add New</button
+					>
+				{/if}
+			</div>
+			<div class="container grid-item">
+				<h1>
+					Rennovation Log <button
+						on:click={() => {
+							rennovationLogDropdown = !rennovationLogDropdown;
+						}}>+</button
+					>
+				</h1>
+				{#if rennovationLogDropdown}
+					{#each $userData.properties[id]['renovation-log'] as renovationLog, i}
+						<div style="display: flex; align-items: center;">
+							<div class="bubble">
+								<div class="bubble-child">
+									<input
+										placeholder="(description)"
+										bind:value={$userData.properties[id]['renovation-log'][i]['description']}
+									/>
+								</div>
+								<div class="bubble-child">
+									<input
+										placeholder="(date)"
+										bind:value={$userData.properties[id]['renovation-log'][i]['date']}
+									/>
+								</div>
+								<div class="bubble-child">
+									$<input
+										placeholder="(cost)"
+										bind:value={$userData.properties[id]['renovation-log'][i]['cost']}
+									/>
+								</div>
+							</div>
+							<button
+								style="height: 50%"
+								on:click={() => {
+									$userData.properties[id]['renovation-log'].splice(i, 1);
+									$userData = $userData;
+								}}>X</button
+							>
+						</div>
+					{/each}
+					<br />
+					<button
+						on:click={() => {
+							$userData.properties[id]['renovation-log'].push({
+								description: '',
+								date: '',
+								cost: ''
+							});
+							$userData = $userData;
+						}}>Add New</button
+					>
+				{/if}
+			</div>
 		</div>
 		<div class="container">
-			<h1>
-				Interior & Exterior <button
-					on:click={() => {
-						featuresDropdown = !featuresDropdown;
-					}}>+</button
-				>
-			</h1>
-			{#if featuresDropdown}
-				{#each $userData.properties[id]['features'] as feature, i}
-					<div style="display: flex; align-items: center;">
-						<div class="bubble">
-							<div class="bubble-child">
-								Name: <input
-									placeholder="(name here)"
-									bind:value={$userData.properties[id]['features'][i]['name']}
-								/>
-							</div>
-							<div class="bubble-child">
-								Type: <input
-									placeholder="(type here)"
-									bind:value={$userData.properties[id]['features'][i]['type']}
-								/>
-							</div>
-							<div class="bubble-child">
-								Description: <input
-									placeholder="(description here)"
-									bind:value={$userData.properties[id]['features'][i]['description']}
-								/>
-							</div>
-						</div>
-						<button
-							style="height: 50%"
-							on:click={() => {
-								$userData.properties[id]['features'].splice(i, 1);
-								$userData = $userData;
-							}}>X</button
-						>
-					</div>
-				{/each}
-				<br />
-				<button
-					on:click={() => {
-						$userData.properties[id]['features'].push({
-							name: null,
-							type: null,
-							description: null
-						});
-						$userData = $userData;
-					}}>Add New</button
-				>
-			{/if}
-		</div>
-		<div class="container">
-			<h1>
-				Defect Log <button
-					on:click={() => {
-						defectLogDropdown = !defectLogDropdown;
-					}}>+</button
-				>
-			</h1>
-
-			{#if defectLogDropdown}
-				{#each $userData.properties[id]['defect-log'] as defectLog, i}
-					<div style="display: flex; align-items: center;">
-						<div class="bubble">
-							<div class="bubble-child">
-								<input
-									placeholder="(description here)"
-									bind:value={$userData.properties[id]['defect-log'][i]['description']}
-								/>
-							</div>
-							<div class="bubble-child">
-								<input
-									placeholder="(date here)"
-									bind:value={$userData.properties[id]['defect-log'][i]['date']}
-								/>
-							</div>
-							<div class="bubble-child">
-								$<input
-									placeholder="(repair cost here)"
-									bind:value={$userData.properties[id]['defect-log'][i]['repair-cost']}
-								/>
-							</div>
-						</div>
-						<button
-							style="height: 50%"
-							on:click={() => {
-								$userData.properties[id]['defect-log'].splice(i, 1);
-								$userData = $userData;
-							}}>X</button
-						>
-					</div>
-				{/each}
-				<br />
-				<button
-					on:click={() => {
-						$userData.properties[id]['defect-log'].push({
-							description: null,
-							date: null,
-							'repair-cost': null
-						});
-						$userData = $userData;
-					}}>Add New</button
-				>
-			{/if}
-		</div>
-		<div class="container">
-			<h1>
-				Rennovation Log <button
-					on:click={() => {
-						rennovationLogDropdown = !rennovationLogDropdown;
-					}}>+</button
-				>
-			</h1>
-			{#if rennovationLogDropdown}
-				{#each $userData.properties[id]['renovation-log'] as renovationLog, i}
-					<div style="display: flex; align-items: center;">
-						<div class="bubble">
-							<div class="bubble-child">
-								<input
-									placeholder="(description)"
-									bind:value={$userData.properties[id]['renovation-log'][i]['description']}
-								/>
-							</div>
-							<div class="bubble-child">
-								<input
-									placeholder="(date)"
-									bind:value={$userData.properties[id]['renovation-log'][i]['date']}
-								/>
-							</div>
-							<div class="bubble-child">
-								$<input
-									placeholder="(cost)"
-									bind:value={$userData.properties[id]['renovation-log'][i]['cost']}
-								/>
-							</div>
-						</div>
-						<button
-							style="height: 50%"
-							on:click={() => {
-								$userData.properties[id]['renovation-log'].splice(i, 1);
-								$userData = $userData;
-							}}>X</button
-						>
-					</div>
-				{/each}
-				<br />
-				<button
-					on:click={() => {
-						$userData.properties[id]['renovation-log'].push({
-							description: null,
-							date: null,
-							cost: null
-						});
-						$userData = $userData;
-					}}>Add New</button
-				>
-			{/if}
-		</div>
-		<div>
 			<h1>Condition Report</h1>
 			<p>condition report contents (to be filled)</p>
-			<h1>Environment Report</h1>
-			{environmentalReport}
+			<h1>Environment</h1>
+			<div class="report-container">
+				{#if environmentalReportPromise}
+					{#await environmentalReportPromise}
+						<div class="loading"><LoadingIcon /></div>
+					{:then data}
+						<Typewriter><p>{data['message']}</p></Typewriter>
+					{:catch error}
+						<p>{error}</p>
+					{/await}
+				{/if}
+			</div>
 			<h1>Narrative</h1>
-			{narrative}
+			<div class="report-container">
+				{#if narrativeReportPromise}
+					{#await narrativeReportPromise}
+						<div class="loading"><LoadingIcon /></div>
+					{:then data}
+						<Typewriter><p>{data['message']}</p></Typewriter>
+					{:catch error}
+						<p>{error}</p>
+					{/await}
+				{/if}
+			</div>
 		</div>
 		<button on:click={postData}>Save Data</button>
 	</div>
 {/if}
 
 <style>
+	@font-face {
+		font-family: 'Montreal';
+		src: url('/fonts/Montreal-Regular.woff') format('woff');
+	}
 	.container {
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		flex-direction: column;
 	}
+	.grid-item {
+	}
+	.dropdown {
+		overflow-y: scroll;
+	}
+	.report-container {
+		padding: 20px;
+		font-family: Montreal, serif;
+		width: 80%;
+	}
+	.loading {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.grid {
+		margin-top: 50px;
+		width: 100vw;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 1fr 1fr;
+	}
+	button {
+		border-radius: 30%;
+		border: none;
+		background-color: transparent;
+		color: white;
+		font-size: 30px;
+	}
+	h1 {
+		display: flex;
+		flex-direction: row;
+	}
 	.bubble {
 		border-radius: 10px;
-		padding: 25px;
+		padding: 10px 5px;
 		margin: 5px;
 		background-color: rgb(82, 129, 133);
+		height: min-content;
 	}
 	.bubble-child {
 		background-color: rgb(82, 129, 133);
@@ -337,7 +413,14 @@
 		border: none;
 		outline: none;
 		/* border-bottom: 1px solid #ccc; You can adjust the color and thickness of the outline */
-		background-color: none;
+		/* background-color: rgb(90, 131, 134); */
+		/* background-color: red; */
+		background-color: transparent;
+		color: white;
+		font-family: Montreal, serif;
+	}
+	input:first-child {
+		background-color: transparent;
 	}
 	.input-title {
 		display: block;
@@ -349,15 +432,12 @@
 		font-weight: bold;
 		text-align: center;
 	}
-	input {
-		background-color: transparent;
-		color: white;
-	}
 	#title-wrapper {
 		display: flex;
 		justify-content: center;
 		flex-direction: column;
 		align-items: center;
+		font-family: Montreal, serif;
 	}
 	img {
 		border-radius: 20px;
